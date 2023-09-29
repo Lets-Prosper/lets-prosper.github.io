814f04e1aa014d60a0bb3e208aaa84a41693ca12
emaimona <emanuel.maimona@instructure.com>
2023-09-30 00:55:04 +0200

    add files

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+let row_data = null;
+
+fetch('development_data.json')
+  .then(response => response.json())
+  .then(data => {
+    row_data = data;
+    processData(row_data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
 const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
-animate();
+function processData(row_data) {
+  current_state = row_data[0];
+  next_state = row_data[1];
+  current_timestamp = current_state.Timestamp;
+  while (next_state != null) {
+    current_timestamp += 1;
+    animate(current_state);
+    if (current_timestamp >= next_state.Timestamp) {
+      current_state = next_state;
+      next_state = row_data[row_data.indexOf(current_state) + 1];
+    }
+    console.log(current_state)
+  }
+}
 
-function animate() {
+
+function animate(current_state) {
+  const ctx = canvas.getContext("2d");
+  const road = new Road(canvas.width / 2, canvas.width * 0.9);
+  const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+  const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
+
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
